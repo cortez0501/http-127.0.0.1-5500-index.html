5dc3dcf28695e4cbcb41d6a48f897c11dc89d327
Mafer Cortez <maferc20190501@gmail.com>
2024-10-21 18:59:31 -0500

    Add files via upload

---
+++ notas.js
@@ -0,0 +1,40 @@
+const nota1 = 90
+
+if(nota1 >= 90){
+    promedio = 'excelente'
+} else if (nota1 <= 60){
+    promedio = 'reprobado'
+}
+
+console.log(promedio)
+
+
+const nota2 = 75
+
+if(nota2 >= 75){
+    promedio = 'bien'
+} else if (nota2 <= 60){
+    promedio = 'reprobado'
+}
+
+console.log(promedio)
+
+const nota3 = 60
+
+if(nota3 >= 60){
+    promedio = 'suficiente'
+} else if (nota3 <= 60){
+    promedio = 'reprobado'
+}
+
+console.log(promedio)
+
+const nota4 = -60
+
+if(nota4 >= 75){
+    promedio = 'reprobado'
+} else if (nota4 <= 60){
+    promedio = 'reprobado'
+}
+
+console.log(promedio)
